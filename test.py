01fb562b087f2ffad8a6d2fde208e149b7747a73
Aurae31 <ldt10ct@gmail.com>
2026-01-13 10:04:45 +0700

    update modeling 3

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from scipy import io
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import io
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
